8d56eb0a7be3c445ecd53aed566815a0186ddcd7
Dhaval Tank <dhavaltank2001@gmail.com>
2024-04-30 21:09:19 +0530

    Add sass file and implement it

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,9 @@
 
 import { defineConfig } from "rollup";
 import typescript from "@rollup/plugin-typescript";
+import styles from 'rollup-plugin-styles';
+import json from '@rollup/plugin-json';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default defineConfig({
     input: "src/index.ts",
@@ -10,5 +13,14 @@ export default defineConfig({
         name: "dhavaltank-ui",
     },
     external: ["react", "react-dom"],
-    plugins: [typescript({ tsconfig: "tsconfig.json" })],
+    plugins: [
+        typescript({ tsconfig: "tsconfig.json" }),
+        styles({
+            mode: 'inject'
+        }),
+        json(),
+        commonjs({
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        }),
+    ],
 });
--- src/components/DUIButton/DUIButton.tsx
+++ src/components/DUIButton/DUIButton.tsx
@@ -1,20 +1,10 @@
 import * as React from "react";
 import { DUIButtonPropsType } from "../../types/index";
+import "../../styles/DUIButton.scss";
 
 const DUIButton = ({ children, onClick }: DUIButtonPropsType) => {
     return (
-        <button
-            onClick={onClick}
-            style={{
-                padding: "10px 20px",
-                fontSize: "1.2em",
-                borderRadius: "5px",
-                cursor: "pointer",
-                backgroundColor: "blue",
-                color: "white",
-                border: "none",
-            }}
-        >
+        <button onClick={onClick} className="DUIButton">
             {children}
         </button>
     );
